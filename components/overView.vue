a50fd3db530972cf372901bc90541fe45bab1c26
SabaKoberidze <koberidzesaba@gmail.com>
2025-02-20 01:30:34 +0400

    overView done 4/4

--- components/overView.vue
+++ components/overView.vue
@@ -3,43 +3,43 @@
     <div class="mainOverViewContainer">
       <div class="overViewContainer">
         <div class="bgImage"></div>
-        <h1 class="overViewTitle" ref="mainTitle">პროფესიონალური აპარატურა {{scrollProgress}}%</h1>
-        <div v-for="(device, index) in devices" 
-          :key="index" 
-          class="device" 
-          :style="{left: `${device.currentPosition.x}%`, top: `${device.currentPosition.y}%`, opacity: `${device.opacity}` }"
-          >
+        <h1 class="overViewTitle" ref="mainTitle">პროფესიონალური აპარატურა
+        </h1>
+        <div v-for="(device, index) in devices" :key="index" class="device"
+          :class="{ enableInteraction: (scrollProgress <= 40) }"
+          :style="{ left: `${device.currentPosition.x}%`, top: `${device.currentPosition.y}%`, opacity: `${device.opacity}` }">
           {{ device.name }}
         </div>
-       <h1 class="examplesHeader" 
-        :style="{
-        left: `${examplesHeader.currentPosition.x}%`, 
-        top: `${examplesHeader.currentPosition.y}%`, 
-        opacity: examplesHeader.opacity, 
-        transform: ` translate(-50%, -50%) scale(${examplesHeader.currentPosition.scale}) `,
-        }"
-       >ჩვენი ჩაწერილი მუსიკა{{scrollProgress}}%</h1>
-        <div class="cardContainer"
-        :style="{
-          left: `${exampleContainer.currentPosition.x}%`, 
-          top: `${exampleContainer.currentPosition.y}%`, 
+        <h1 class="examplesHeader" :style="{
+          left: `${examplesHeader.currentPosition.x}%`,
+          top: `${examplesHeader.currentPosition.y}%`,
+          opacity: examplesHeader.opacity,
+          transform: ` translate(-50%, -50%) scale(${examplesHeader.currentPosition.scale}) `,
+        }">ჩვენი ჩაწერილი მუსიკა</h1>
+        <div :class="{ enableInteraction: (scrollProgress >= 90) }" class="cardContainer" :style="{
+          left: `${exampleContainer.currentPosition.x}%`,
+          top: `${exampleContainer.currentPosition.y}%`,
           opacity: exampleContainer.opacity
-          }"
-        >
-          <div class="card" v-for="(example, index) in examples">
-            <div class="cardHolder">
-              <img class="vinyl" src="/images/examples/vinyl.png"/>
-              <div class="cardInnerContainer">
-                <img class="cardImage" :src="`images/examples/${example.title}.png`"/>       
-                <div class="cardHole"></div> 
-                <p>
-                  {{ example.title }}
-                </p>
-              </div>
+        }">
+          <div class="card" v-for="(example, index) in examples" :style="{
+            left: `${example.currentPosition.x}%`,
+            top: `${example.currentPosition.y}%`,
+            'z-index': example.zIndex,
+            transform: `translate(-50%, -50%) rotate(${example.currentPosition.rotation}deg) scale(${example.currentPosition.scale})`
+          }">
+            <div class="cardHolder"">
+              <img class=" vinyl" src="/images/examples/vinyl.png" />
+            <div class="cardInnerContainer">
+              <img class="cardImage" :src="`images/examples/${example.title}.png`" />
+              <div class="cardHole"></div>
+              <p :class="{ showTitle: (scrollProgress > 90) }">
+                {{ example.title }}
+              </p>
             </div>
           </div>
         </div>
       </div>
+    </div>
     </div>
   </article>
 </template>
@@ -52,7 +52,7 @@ let scrollTop = 0;
 let maxScroll = 0;
 const scrollProgress = ref(0)
 const lastProgress = ref(0)
-const mainTitle = ref(null)
+const mainTitle = ref<HTMLElement | null>(null);
 const exampleTitle = ref(null)
 
 let progressIndexes: number[] = [0, 30, 45, 60, 100]
@@ -95,38 +95,59 @@ const devices = ref([
   },
 ]);
 const examplesHeader = ref({
-  startPosition: { x: 50,  y: 100, scale: 1},
-  currentPosition: {x: 50, y: 100, scale: 1},
-  middlePosition: {x:50,  y: 50, scale: 1},
-  endPosition: {x: 50, y: 20, scale: 0.6},
+  startPosition: { x: 50, y: 100, scale: 1 },
+  currentPosition: { x: 50, y: 100, scale: 1 },
+  middlePosition: { x: 50, y: 50, scale: 1 },
+  endPosition: { x: 50, y: 20, scale: 0.6 },
   opacity: 0
 })
 const exampleContainer = ref({
-  startPosition: JSON.parse(JSON.stringify(examplesHeader.value.startPosition)), 
-  endPosition: JSON.parse(JSON.stringify(examplesHeader.value.middlePosition)),   
-  currentPosition: {x: 50, y: 100},
+  startPosition: JSON.parse(JSON.stringify(examplesHeader.value.startPosition)),
+  endPosition: JSON.parse(JSON.stringify(examplesHeader.value.middlePosition)),
+  currentPosition: { x: 50, y: 100 },
+  opacity: 0
 })
 const examples = ref([
   {
-   title: 'Punk' 
+    title: 'Punk',
+    startPosition: { x: 45, y: 50, scale: 1.9, rotation: -16 },
+    endPosition: { x: 16, y: 50, scale: 1, rotation: 0 },
+    currentPosition: { x: 45, y: 50, scale: 1.9, rotation: -16 },
+    zIndex: 1,
   },
   {
-   title: 'Jazz' 
+    title: 'Jazz',
+    startPosition: { x: 47, y: 48, scale: 2, rotation: -10 },
+    endPosition: { x: 33, y: 50, scale: 1, rotation: 0 },
+    currentPosition: { x: 47, y: 48, scale: 2, rotation: -10 },
+    zIndex: 2,
   },
   {
-   title: 'Metal' 
+    title: 'Metal',
+    startPosition: { x: 50, y: 48, scale: 2.3, rotation: 0 },
+    endPosition: { x: 50, y: 47, scale: 1.3, rotation: 0 },
+    currentPosition: { x: 50, y: 48, scale: 2.3, rotation: 0 },
+    zIndex: 3,
   },
   {
-   title: 'Blues' 
+    title: 'Blues',
+    startPosition: { x: 53, y: 48, scale: 2, rotation: 10 },
+    endPosition: { x: 67, y: 50, scale: 1, rotation: 0 },
+    currentPosition: { x: 53, y: 48, scale: 2, rotation: 10 },
+    zIndex: 2,
   },
   {
-   title: 'Rock' 
+    title: 'Rock',
+    startPosition: { x: 55, y: 50, scale: 1.9, rotation: 16 },
+    endPosition: { x: 83, y: 50, scale: 1, rotation: 0 },
+    currentPosition: { x: 55, y: 50, scale: 1.9, rotation: 16 },
+    zIndex: 1,
   },
 ])
 
 
 const isHovered = ref(false)
-function handleScroll(event) {
+function handleScroll() {
   if (scrollContainer.value) {
     const containerTop = scrollContainer.value.offsetTop;
     const currentScroll = window.scrollY - containerTop;
@@ -141,105 +162,147 @@ function handleScroll(event) {
       return
     }
     scrollProgress.value = Math.round(scrollProgress.value)
-    if(scrollProgress.value !== lastProgress.value){
+    if (scrollProgress.value !== lastProgress.value) {
       scrollAnimation()
     }
     lastProgress.value = scrollProgress.value
   }
 };
 function scrollAnimation() {
-  if (scrollProgress.value >= progressIndexes[0] && scrollProgress.value <= progressIndexes[1]) { 
-    const scale = 1 - ((scrollProgress.value - progressIndexes[0]) / (progressIndexes[2] - progressIndexes[0])); 
-    mainTitle.value.style.transform = `scale(${scale})`;
-    moveDevices(0,2)
+  if (scrollProgress.value >= progressIndexes[0] && scrollProgress.value <= progressIndexes[1]) {
+    const scale = 1 - ((scrollProgress.value - progressIndexes[0]) / (progressIndexes[2] - progressIndexes[0]));
+    if (mainTitle.value) {
+      mainTitle.value.style.transform = `scale(${scale})`;
+    }
+    moveDevices(0, 2)
 
     examplesHeader.value.opacity = 0
-  }else if(scrollProgress.value >= progressIndexes[1] && scrollProgress.value <= progressIndexes[2]) {
-    const scale = 1 - ((scrollProgress.value - progressIndexes[0]) / (progressIndexes[2] - progressIndexes[0])); 
-    mainTitle.value.style.transform = `scale(${scale})`;
-    moveDevices(0,2)
+  } else if (scrollProgress.value >= progressIndexes[1] && scrollProgress.value <= progressIndexes[2]) {
+    const scale = 1 - ((scrollProgress.value - progressIndexes[0]) / (progressIndexes[2] - progressIndexes[0]));
+    if (mainTitle.value) {
+      mainTitle.value.style.transform = `scale(${scale})`;
+    }
+    moveDevices(0, 2)
     moveText(1, 3, 1)
     moveCardContainer(1, 3)
   }
   else if (scrollProgress.value >= progressIndexes[2] && scrollProgress.value <= progressIndexes[3]) {
-    mainTitle.value.style.transform = `scale(0)`;
+    if (mainTitle.value) {
+      mainTitle.value.style.transform = `scale(0)`;
+    }
     devices.value.forEach(device => {
       device.currentPosition.x = device.endPosition.x
       device.currentPosition.y = device.endPosition.y
       device.opacity = 0
     })
+    exampleContainer.value.opacity = 1
+    examples.value.forEach(example => {
+      example.currentPosition.x = example.startPosition.x
+      example.currentPosition.y = example.startPosition.y
+      example.currentPosition.scale = example.startPosition.scale
+      example.currentPosition.rotation = example.startPosition.rotation
+    })
     moveText(1, 3, 1)
     moveCardContainer(1, 3)
+
   }
   else if (scrollProgress.value >= progressIndexes[3] && scrollProgress.value <= progressIndexes[4]) {
-    mainTitle.value.style.transform = `scale(0)`;
+    if (mainTitle.value) {
+      mainTitle.value.style.transform = `scale(0)`;
+    }
+    exampleContainer.value.opacity = 1
+    exampleContainer.value.currentPosition.y = exampleContainer.value.endPosition.y
     devices.value.forEach(device => {
       device.currentPosition.x = device.endPosition.x
       device.currentPosition.y = device.endPosition.y
       device.opacity = 0
     })
     moveText(3, 4, 2)
+    spreadCards(3, 4)
   }
 }
 
-function moveDevices(startIndex, endIndex){
-  devices.value.forEach(device => {   
-      device.currentPosition.x = device.startPosition.x + 
-        (device.endPosition.x - device.startPosition.x) * AnimationProgress(startIndex, endIndex);
-      
-      device.currentPosition.y = device.startPosition.y + 
-        (device.endPosition.y - device.startPosition.y) * AnimationProgress(startIndex, endIndex);
-      
-        if(scrollProgress.value > progressIndexes[2] / 4){
-          device.opacity = 1
-        }
-        else{
-          device.opacity = 0
-        }
-    });
+function moveDevices(startIndex: number, endIndex: number) {
+  devices.value.forEach(device => {
+    device.currentPosition.x = device.startPosition.x +
+      (device.endPosition.x - device.startPosition.x) * AnimationProgress(startIndex, endIndex);
+
+    device.currentPosition.y = device.startPosition.y +
+      (device.endPosition.y - device.startPosition.y) * AnimationProgress(startIndex, endIndex);
+
+    if (scrollProgress.value > progressIndexes[2] / 4) {
+      device.opacity = 1
+    }
+    else {
+      device.opacity = 0
+    }
+  });
 }
-function moveText(startIndex, endIndex, stage) {
+function moveText(startIndex: number, endIndex: number, stage: number) {
   const { startPosition, middlePosition, endPosition } = examplesHeader.value;
-  const [start, end] = stage === 1 
-    ? [startPosition, middlePosition] 
+  const [start, end] = stage === 1
+    ? [startPosition, middlePosition]
     : [middlePosition, endPosition];
   const { x: startPositionX, y: startPositionY, scale: startScale } = start;
   const { x: endPositionX, y: endPositionY, scale: endScale } = end;
 
 
-  examplesHeader.value.currentPosition.x = startPositionX + 
+  examplesHeader.value.currentPosition.x = startPositionX +
     (endPositionX - startPositionX) * AnimationProgress(startIndex, endIndex);
-  examplesHeader.value.currentPosition.y = startPositionY + 
+  examplesHeader.value.currentPosition.y = startPositionY +
     (endPositionY - startPositionY) * AnimationProgress(startIndex, endIndex);
-  examplesHeader.value.currentPosition.scale = startScale + 
+  examplesHeader.value.currentPosition.scale = startScale +
     (endScale - startScale) * AnimationProgress(startIndex, endIndex);
 
-  if(scrollProgress.value > progressIndexes[2] - progressIndexes[2] / 8){
+  if (scrollProgress.value > progressIndexes[2] - progressIndexes[2] / 8) {
     examplesHeader.value.opacity = 1
   }
-  else{
+  else {
     examplesHeader.value.opacity = 0
-  }    
+  }
 }
-function moveCardContainer(startIndex, endIndex){
+
+function spreadCards(startIndex: number, endIndex: number) {
+  examples.value.forEach((example) => {
+    const { startPosition, endPosition } = example;
+    const { x: startPositionX, y: startPositionY, scale: startScale, rotation: startRotation } = startPosition;
+    const { x: endPositionX, y: endPositionY, scale: endScale, rotation: endRotation } = endPosition;
+
+    example.currentPosition.x = startPositionX +
+      (endPositionX - startPositionX) * AnimationProgress(startIndex, endIndex);
+
+    example.currentPosition.y = startPositionY +
+      (endPositionY - startPositionY) * AnimationProgress(startIndex, endIndex);
+
+    example.currentPosition.scale = startScale +
+      (endScale - startScale) * AnimationProgress(startIndex, endIndex);
+
+    example.currentPosition.rotation = startRotation +
+      (endRotation - startRotation) * AnimationProgress(startIndex, endIndex);
+  })
+}
+
+function moveCardContainer(startIndex: number, endIndex: number) {
   const { startPosition, endPosition } = exampleContainer.value;
   const { x: startPositionX, y: startPositionY } = startPosition;
   const { x: endPositionX, y: endPositionY } = endPosition;
 
-  exampleContainer.value.currentPosition.x = startPositionX + 
-        (endPositionX - startPositionX) * AnimationProgress(startIndex, endIndex);
-      
-  exampleContainer.value.currentPosition.y = startPositionY + 
+
+  exampleContainer.value.currentPosition.x = startPositionX +
+    (endPositionX - startPositionX) * AnimationProgress(startIndex, endIndex);
+
+  exampleContainer.value.currentPosition.y = startPositionY +
     (endPositionY - startPositionY) * AnimationProgress(startIndex, endIndex);
 
-  if(scrollProgress.value > progressIndexes[2] - progressIndexes[2] / 8){
+  if (scrollProgress.value > progressIndexes[2] - progressIndexes[2] / 8) {
     exampleContainer.value.opacity = 1
   }
-  else{
+  else {
     exampleContainer.value.opacity = 0
-  }    
+  }
+
 }
-function AnimationProgress(startIndex, endIndex){
+function AnimationProgress(startIndex: number, endIndex: number) {
   return ((scrollProgress.value - progressIndexes[startIndex]) / (progressIndexes[endIndex] - progressIndexes[startIndex]))
 }
 
@@ -276,7 +339,8 @@ article {
     margin: 0;
     padding: 0;
   }
-  .examplesHeader{
+
+  .examplesHeader {
     position: absolute;
     font-family: "SF Georgian";
     font-feature-settings: 'case';
@@ -286,26 +350,37 @@ article {
     width: 1088px;
     text-align: center;
     transition: 200ms;
+    z-index: 10;
+    pointer-events: none;
   }
-  .cardContainer{
+
+  .cardContainer {
     display: flex;
     width: 100%;
+    height: 100%;
     position: absolute;
     justify-content: center;
     align-items: center;
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
-    transition: 400ms;
-  
-    .card{
+    transition: 200ms;
+    pointer-events: none;
+    z-index: 5;
+
+    &.enableInteraction {
+      pointer-events: unset;
+    }
+
+    .card {
       display: flex;
       flex-direction: column;
       justify-content: center;
       align-items: center;
-      width: 12%;
       transition: 200ms;
-      .cardHolder{
+      position: absolute;
+
+      .cardHolder {
         width: 180px;
         height: 180px;
         display: flex;
@@ -314,24 +389,30 @@ article {
         justify-content: center;
         position: relative;
         transition: 200ms;
-        .vinyl{
+        border-radius: 50%;
+        box-shadow: 0px 0px 15px black;
+
+        .vinyl {
           position: absolute;
           width: 100%;
           height: 100%;
           z-index: -1;
           transition: 200ms;
         }
-        .cardInnerContainer{
+
+        .cardInnerContainer {
           width: 100%;
           height: 100%;
           overflow: hidden;
           transition: 200ms;
           outline: 0px solid #000000;
-          .cardImage{
+
+          .cardImage {
             width: 100%;
             height: 100%;
           }
-          .cardHole{
+
+          .cardHole {
             width: 0;
             height: 0;
             position: absolute;
@@ -343,30 +424,40 @@ article {
             transition: 200ms;
             border-radius: 50%;
           }
-          p{
+
+          p {
             position: absolute;
             top: calc(100% + 20px);
             left: 50%;
             transform: translateX(-50%);
+            opacity: 0;
+            transition: 200ms;
+
+            &.showTitle {
+              opacity: 1;
+            }
           }
+
         }
-  
-        &:hover{
+
+        &:hover {
           cursor: pointer;
           width: 240px;
           height: 240px;
-          .cardInnerContainer{
+
+          .cardInnerContainer {
             width: 51%;
             height: 51%;
             border-radius: 50%;
             outline: 3px solid #952532;
-            .cardHole{
+
+            .cardHole {
               width: 16px;
               height: 16px;
               border: 2px solid #952532;
             }
           }
-      }
+        }
 
       }
     }
@@ -407,7 +498,8 @@ article {
     text-align: center;
     transition: 200ms;
   }
-  .device{
+
+  .device {
     display: flex;
     width: 180px;
     height: 220px;
@@ -423,7 +515,13 @@ article {
     transform: translate(-50%, -50%);
     background: radial-gradient(125.76% 125.76% at 50% 145.45%, rgba(245, 194, 92, 0) 0%, rgba(0, 0, 0, 0.00) 100%), linear-gradient(180deg, rgba(23, 24, 25, 0.00) 0%, rgba(23, 24, 25, 0.80) 100%), #000;
     transition: 200ms;
-    &:hover{
+    pointer-events: none;
+
+    &.enableInteraction {
+      pointer-events: unset;
+    }
+
+    &:hover {
       cursor: pointer;
       background: radial-gradient(125.76% 125.76% at 50% 145.45%, rgba(245, 195, 92, 0.30) 0%, rgba(0, 0, 0, 0.00) 100%), linear-gradient(180deg, rgba(23, 24, 25, 0.00) 0%, rgba(23, 24, 25, 0.80) 100%), #000;
     }
